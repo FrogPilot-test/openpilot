cb32303ba1419fa3d8110dbacd408bbc89bddad8
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-07-14 12:38:06 -0700

    Custom road UI
    Added toggles to customize the lane lines, path, road edges, path edges, and add a blind spot path along with an "Unlimited Length" mode that extends the road UI out as far as the model can see.

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -77,6 +77,8 @@ class AnnotatedCameraWidget : public CameraWidget {
   Q_PROPERTY(int status MEMBER status);
 
   // FrogPilot properties
+  Q_PROPERTY(bool blindSpotLeft MEMBER blindSpotLeft);
+  Q_PROPERTY(bool blindSpotRight MEMBER blindSpotRight);
   Q_PROPERTY(bool experimentalMode MEMBER experimentalMode);
   Q_PROPERTY(bool frogColors MEMBER frogColors);
   Q_PROPERTY(bool muteDM MEMBER muteDM);
@@ -118,6 +120,8 @@ private:
   bool wide_cam_requested = false;
 
   // FrogPilot variables
+  bool blindSpotLeft;
+  bool blindSpotRight;
   bool experimentalMode;
   bool frogColors;
   bool muteDM;
