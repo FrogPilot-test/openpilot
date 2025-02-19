67c5b7cffd549bbb137b92a00168b9432c97fcc6
FrogPilot-test <142936991+FrogPilot-test@users.noreply.github.com>
2025-02-19 12:36:13 -0600

    Attempt steer to 0

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -60,7 +60,7 @@ class CarController(CarControllerBase):
         lkas_control_bit = True
       elif self.CP.flags & ChryslerFlags.HIGHER_MIN_STEERING_SPEED:
         if CS.out.vEgo < (self.CP.minSteerSpeed - 3.0):
-          lkas_control_bit = False
+          lkas_control_bit = True
       elif self.CP.carFingerprint in RAM_CARS:
         if CS.out.vEgo < (self.CP.minSteerSpeed - 0.5):
           lkas_control_bit = False
