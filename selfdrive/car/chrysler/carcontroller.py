4148543e05be4f587b53be423a85cc69e0bc11d7
FrogPilot-test <142936991+FrogPilot-test@users.noreply.github.com>
2025-02-19 12:51:57 -0600

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -59,8 +59,7 @@ class CarController(CarControllerBase):
       elif CS.out.vEgo > self.CP.minSteerSpeed:
         lkas_control_bit = True
       elif self.CP.flags & ChryslerFlags.HIGHER_MIN_STEERING_SPEED:
-        if CS.out.vEgo < (self.CP.minSteerSpeed - 3.0):
-          lkas_control_bit = True
+        lkas_control_bit = True
       elif self.CP.carFingerprint in RAM_CARS:
         if CS.out.vEgo < (self.CP.minSteerSpeed - 0.5):
           lkas_control_bit = False
